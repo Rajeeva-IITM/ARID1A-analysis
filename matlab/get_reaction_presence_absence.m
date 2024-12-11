da5358cffe3d2795b7f8114168d3065110943839
Rajeeva-IITM <bt20d204@smail.iitm.ac.in>
2024-12-11 23:56:12 +0530

    New model evaluation after meeting on 11-12-24

---
+++ matlab/get_reaction_presence_absence.m
@@ -0,0 +1,52 @@
+% Code for creating binary reaction matrices from the built models
+
+%% Preliminaries
+clear
+loadenv("./.env")  %Loading the environment variables
+addpath(getenv("COBRATOOLBOX_PATH"))
+
+%% Data
+
+recon3d = readCbModel(strcat('./data/', '/models/Recon3DModel_301.mat'));  % Consistent Recon3D model
+
+all_reactions = recon3d.rxns;
+
+% Getting path to all the models
+
+sprint_model_paths = cellstr(ls("outputs\builtmodels\localgini_sprintcore_avg\*.mat"));
+gimme_model_paths = cellstr(ls("outputs\builtmodels\localgini_gimme_avg\*.mat"));
+init_model_paths = cellstr(ls("outputs\builtmodels\localgini_init_avg\*.mat"));
+
+result_table = table(all_reactions, 'VariableNames', {'Reactions'});
+
+for i=1:length(sprint_model_paths)
+    
+    % Some calculation for getting the column names
+    sprint_model_name = strsplit(sprint_model_paths{i}, '.');
+    sprint_model_name = ['sprint_' sprint_model_name{1}];
+
+    gimme_model_name = strsplit(gimme_model_paths{i}, '.');
+    gimme_model_name = ['gimme_' gimme_model_name{1}];
+
+    init_model_name = strsplit(init_model_paths{i}, '.');
+    init_model_name = ['init_' init_model_name{1}];
+    
+    % Get the reactions in the context specific models and then see which
+    % reactions are present and which aren't
+
+    sprint_model = readCbModel(['outputs\builtmodels\localgini_sprintcore_avg\' sprint_model_paths{i}]);
+    sprint_rxns = ismember(all_reactions, sprint_model.rxns);
+
+    gimme_model = readCbModel(['outputs\builtmodels\localgini_gimme_avg\' gimme_model_paths{i}]);
+    gimme_rxns = ismember(all_reactions, gimme_model.rxns);
+
+    init_model = readCbModel(['outputs\builtmodels\localgini_init_avg\' init_model_paths{i}]);
+    init_rxns = ismember(all_reactions, init_model.rxns);
+
+    result_table.(sprint_model_name) = sprint_rxns;
+    result_table.(gimme_model_name) = gimme_rxns;
+    result_table.(init_model_name) = init_rxns;
+
+end
+
+writetable(result_table, "outputs\builtmodels\reaction_presence_absence matrix.csv")
